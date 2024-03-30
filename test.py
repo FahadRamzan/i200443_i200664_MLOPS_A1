41363014340640076642e3c881f038aa36384495
Rumaisa Ilyas <i200664@nu.edu.pk>
2024-03-30 16:47:21 +0500

    Updated test.py

--- test.py
+++ test.py
@@ -1,27 +1,33 @@
+import pytest
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from joblib import dump, load
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from joblib import load
 
 
-# Load your CSV data into a pandas DataFrame
-data = pd.read_csv('bank_cleaned.csv')
+@pytest.fixture
+def trained_model():
+    # Load the trained model from the file
+    knn_model = load('knn_model.joblib')
+    return knn_model
 
-# Split the data into features and target variable
-X = data.drop('deposit', axis=1)  # Assuming 'target_column' is the name of your target variable
-y = data['deposit']
+@pytest.fixture
+def test_data():
+    # Load test data
+    data = pd.read_csv('bank_cleaned.csv')
+    X_test = data.drop('deposit', axis=1)
+    y_test = data['deposit']
+    return X_test, y_test
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+def test_accuracy(trained_model, test_data):
+    knn_model = trained_model
+    X_test, y_test = test_data
+    # Make predictions
+    y_pred = knn_model.predict(X_test)
+    # Calculate accuracy
+    accuracy = accuracy_score(y_test, y_pred)
+    # Check if accuracy is not None and between 0 and 1
+    assert accuracy is not None
+    assert 0 <= accuracy <= 1
 
-# Initialize your machine learning model
-# Load the model
-knn_model = load('knn_model.joblib')
-
-
-# Make predictions on the test set
-y_pred = knn_model.predict(X_test)
-
-# Evaluate the model's performance
-accuracy = accuracy_score(y_test, y_pred)
-print(f'Accuracy: {accuracy}')
